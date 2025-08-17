1c9f4daffc7a9e7a2a4ca4d718fbd972b7881b83
Raviteja2000-design <ravitejaghantasala2000@gmail.com>
2025-08-17 15:23:16 +0530

    Update two_ff_sync.v

--- two_ff_sync.v
+++ two_ff_sync.v
@@ -1,12 +1,6 @@
-// -----------------DESCRIPTION------------------
-// This is a 2-stage synchronous FIFO module.
-// ----------------------------------------------
 
-// -------------PARAMETERS------------
-// SIZE: Size of the input data bus
-// -----------------------------------
 
-module two_ff_sync #(parameter SIZE = 4)( 
+module two_ff_sync #(parameter SIZE = 8)( 
     output reg [SIZE-1:0] q2,   // Output of the second flip-flop
     input [SIZE-1:0] din,       // Input data
     input clk, rst_n            // Clock and reset
@@ -23,11 +17,4 @@ module two_ff_sync #(parameter SIZE = 4)(
 
 endmodule
 
-// ---------------------------EXPLANATION---------------------------------
-// The module has two flip-flops, q1 and q2, which store the input data. 
-// On each clock cycle, the data is shifted from q1 to q2, and new data 
-// is loaded into q1. The reset signal (rst_n) is active low, meaning the 
-// FIFO is reset when rst_n is low.  
-// -----------------------------------------------------------------------
- 
 
